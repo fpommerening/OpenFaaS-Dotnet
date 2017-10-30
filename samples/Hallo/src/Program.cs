ac3ed9f7ecb230b8bee9fd9c6aea300084a48c15
Frank Pommerening <frank@pommerening-online.de>
2017-10-30 09:21:13 +0100

    add sample hallo

---
+++ samples/Hallo/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FP.OpenfaasDotnet.Hallo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hallo OpenFAAS {DateTime.Now}");
+        }
+    }
+}
