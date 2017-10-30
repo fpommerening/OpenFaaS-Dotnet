e525e98233c397fb945cbac66caec862bd848220
Frank Pommerening <frank@pommerening-online.de>
2017-10-30 09:19:07 +0100

    add sample console args

---
+++ samples/ConsoleArgs/src/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FP.OpenfaasDotnet.ConsoleArgs
+{
+    class Program
+    {
+            private static string[] GetConsoleInput() {
+            var buffer = new  List<string>();
+            string s;
+            while ((s = Console.ReadLine()) != null)
+            {
+                buffer.Add(s);
+            }
+            return buffer.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+              var buffer = GetConsoleInput();
+            Console.WriteLine(string.Join(Environment.NewLine, buffer));
+        }
+    }
+}
