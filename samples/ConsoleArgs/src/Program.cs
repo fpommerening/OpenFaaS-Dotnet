04b4b8d2ca2b0dbba472401e07b11165cec3cc22
Frank Pommerening <frank@pommerening-online.de>
2017-11-01 15:26:51 +0100

    prepare starter

--- samples/ConsoleArgs/src/Program.cs
+++ samples/ConsoleArgs/src/Program.cs
@@ -5,20 +5,11 @@ namespace FP.OpenfaasDotnet.ConsoleArgs
 {
     class Program
     {
-            private static string[] GetConsoleInput() {
-            var buffer = new  List<string>();
-            string s;
-            while ((s = Console.ReadLine()) != null)
-            {
-                buffer.Add(s);
-            }
-            return buffer.ToArray();
-        }
+           
 
         static void Main(string[] args)
         {
-              var buffer = GetConsoleInput();
-            Console.WriteLine(string.Join(Environment.NewLine, buffer));
+              
         }
     }
 }
--- samples/WebApp/src/Module/HomeModule.cs
+++ samples/WebApp/src/Module/HomeModule.cs
@@ -7,16 +7,7 @@ namespace FP.OpenfaasDotnet.BuildInside.Module
     {
         public HomeModule()
         {
-            Post("/", async (args, ct) =>
-            {
-                string content;
-                using (Stream responseStream = this.Request.Body)
-                using (StreamReader sr = new StreamReader(responseStream))
-                {
-                    content = await sr.ReadToEndAsync();
-                }
-                return $"content received {DateTime.Now}:{System.Environment.NewLine}{content}";
-            });
+           
         }
     }
 }
