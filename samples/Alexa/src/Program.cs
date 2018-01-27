398470f6c08dad10fecc6df9235dd7de733577ad
Frank Pommerening <frank@pommerening-online.de>
2018-01-27 12:44:23 +0100

    Alexa-Sample as console app

--- samples/Alexa/src/Program.cs
+++ samples/Alexa/src/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace FP.OpenfaasDotnet.Alexa
 {
@@ -7,12 +11,79 @@ namespace FP.OpenfaasDotnet.Alexa
     {
         static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var content = Common.StdInHelper.GetValue();
+            var request = JsonConvert.DeserializeObject<SkillRequest>(content);
+
+            SkillResponse response;
+
+            switch (request.Request.Type)
+            {
+                case "IntentRequest":
+                    response = ExecuteIntent(request);
+                    break;
+                case "SessionEndedRequest":
+                    response = CreatePlaneTextResponse("Meetup zu Ende - Auf Wiedersehen beim nächsten Mal.");
+                    break;
+                default:
+                    response = CreatePlaneTextResponse("Willkommen zum Meetup. Was darf ich für dich tun?");
+                    break;
+            }
+
+            JsonConvert.SerializeObject(response, Formatting.Indented,
+                new JsonSerializerSettings {ContractResolver = new CamelCasePropertyNamesContractResolver()});
         }
 
-          public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        private static SkillResponse ExecuteIntent(SkillRequest skillRequest)
+        {
+            var intentRequest = skillRequest.Request as IntentRequest;
+
+            if (intentRequest == null)
+            {
+                return CreateUnkownIntentResponse(string.Empty);
+            }
+            switch (intentRequest.Intent.Name.ToLowerInvariant())
+            {
+                case "greeting":
+                    return CreatePlaneTextResponse("Hallo und Willkommen bei der dot NET Usergroup Leipzig. Ich bin Alexa und wünsche euch viel Spaß beim Vortrag Serverless Computing mit OpenFaaS.");
+                case "sendoff":
+                    return CreatePlaneTextResponse("Vielen Dank für eure Teilnahme und bis zum nächsten mal.");
+                case "breaknow":
+                    return CreatePlaneTextResponse("Pause");
+                case "amazon.helpintent":
+                    return CreatePlaneTextResponse("Wenn du Hilfe brauchst fragt den lieben Gott");
+                case "amazon.stopintent":
+                    return CreatePlaneTextResponse("Meetup aus - OK");
+                default:
+                    return CreateUnkownIntentResponse(intentRequest.Intent.Name);
+            }
+        }
+
+
+
+        private static SkillResponse CreateUnkownIntentResponse(string name)
+        {
+            return CreatePlaneTextResponse($"Dein Befehl {name} wurde nicht gefunden.");
+        }
+
+        private static SkillResponse CreatePlaneTextResponse(string content)
+        {
+            var reponse = new SkillResponse
+            {
+                Version = "1.0",
+
+            };
+
+            reponse.Response = new ResponseBody
+            {
+                OutputSpeech = new PlainTextOutputSpeech
+                {
+                    Text = content
+                }
+            };
+
+            return reponse;
+        }
+
+
     }
 }
