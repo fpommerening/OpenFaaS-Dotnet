77c6a89692e4341cc026e6898401eb9f8694c25b
Frank Pommerening <frank@pommerening-online.de>
2018-02-06 20:17:46 +0100

    - remove Link to data - Add mongodb test

--- samples/CQRS/AddConference/FunctionHandler.cs
+++ samples/CQRS/AddConference/FunctionHandler.cs
@@ -8,9 +8,9 @@ namespace Function
         public void Handle(string input)
         {
             Console.WriteLine("Hi there - your input was: "+ input);
+            // try Mongo-Client
+            var client = new MongoDB.Driver.MongoClient("mongodb://localhost:27017");
 
-            var dl = new Data.DataLayer();
-            dl.SaveEventData(input);
         }
     }
 }
