584e22c1c96eaa3c12432856cbfcb3516cb7c1a7
Frank Pommerening <frank@pommerening-online.de>
2018-02-06 19:34:04 +0100

    link datalayer with add Conference - function

--- samples/CQRS/AddConference/FunctionHandler.cs
+++ samples/CQRS/AddConference/FunctionHandler.cs
@@ -5,8 +5,12 @@ namespace Function
 {
     public class FunctionHandler
     {
-        public void Handle(string input) {
+        public void Handle(string input)
+        {
             Console.WriteLine("Hi there - your input was: "+ input);
+
+            var dl = new Data.DataLayer();
+            dl.SaveEventData(input);
         }
     }
 }
--- samples/CQRS/Data/DataLayer.cs
+++ samples/CQRS/Data/DataLayer.cs
@@ -4,5 +4,9 @@ namespace Data
 {
     public class DataLayer
     {
+        public void SaveEventData<T>(T eventData) where T : class
+        {
+            // Logic with MongoDB ... comes later
+        }
     }
 }
