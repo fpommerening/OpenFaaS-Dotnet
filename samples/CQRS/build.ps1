9293f27dd3107fab583277343191b7d4566e4cd0
Frank Pommerening <frank@pommerening-online.de>
2018-02-20 21:23:00 +0100

    powershell skripte

--- samples/CQRS/build.ps1
+++ samples/CQRS/build.ps1
@@ -1,1 +1,9 @@
-docker build -t csharp-common ./template/csharp-common/Common.Dockerfile .
+
+$cli_path = "C:\OpenFaaS\faas-cli.exe"
+
+& docker build -t csharp-common ./template/csharp-common/Common.Dockerfile .
+
+$stack_file = "stack.yml"
+
+# $stack_file erste Datei *.yml
+& $cli_path build -f $stack_file
--- samples/CQRS/new.ps1
+++ samples/CQRS/new.ps1
@@ -1,1 +1,14 @@
+param([string]$ProjectName='')
+
 $cli_path = "C:\OpenFaaS\faas-cli.exe"
+
+& $cli_path  'new' $ProjectName --lang csharp-common --append stack.yml
+
+# if not exists
+& dotnet new sln 
+
+& dotnet new classlib -n $ProjectName -lang csharp
+& dotnet sln add ./$ProjectName/$ProjectName.csproj
+& Remove-Item ./$ProjectName/Class1.cs
+
+
